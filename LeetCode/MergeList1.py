79a17b5d345f183c09d5c0e7be1b5526862ff124
DMGamage <gamagemadhushan@gmail.com>
2024-04-07 19:38:22 +0530

    Merge Two Sorted Lists

---
+++ LeetCode/MergeList1.py
@@ -0,0 +1,26 @@
+# Q-21 LeetCode
+# Merge Two Sorted Lists
+
+def mergeTwoSort(self,l1:ListNode,l2:ListNode)->ListNode:
+
+    dummy = ListNode()
+    tail = dummy
+
+    while l1 and l2:
+
+        if l1.val < l2.val :
+            tail.next = l1
+            l1 = l1.next
+
+        else:
+            tail.next = l2
+            l2 =l2.next
+        tail = tail.next
+
+    if l1:
+        tail.next = l1
+    elif l2:
+        tail.next = l2
+
+    return dummy.next
+
